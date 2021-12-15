14eb62b355178665b307b1e9c6eff40d6d5b0d64
SKris98 <sereginakris@yandex.ru>
2021-12-15 23:44:43 +0300

    hot fix

--- DinnerAdvisor.py
+++ DinnerAdvisor.py
@@ -1,14 +1,14 @@
 class DinnerAdvisor:
     @staticmethod
     def getAdvice(moneyBeforeSalary: float, daysBeforeSalary: int):
-        if moneyBeforeSalary < 3000:
+        if moneyBeforeSalary <= 3000:
             print("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!")
-        elif moneyBeforeSalary < 10000:
+        elif moneyBeforeSalary <= 10000:
             if daysBeforeSalary < 10:
                 print("Можете шиковать! Пора в Макдак!")
             else:
                 print("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!")
-        elif moneyBeforeSalary < 30000:
+        elif moneyBeforeSalary <= 30000:
             if daysBeforeSalary < 10:
                 print("Замечательно! Можно прикупить долларов и зайти поужинать в классное место. Так держать! :)")
             else:
